ce9e31739391ba9617137a3ac658974edf99f238
Daniel Barclay <barclaysd@me.com>
2020-12-31 19:52:02 +0000

    Add module-alias support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+#!/usr/bin/env node
+import moduleAlias from 'module-alias';
+if (process.env.NODE_ENV !== 'development') {
+  moduleAlias();
+}
 import { readdirSync, readFileSync } from 'fs';
 import { PackageJson, ReleaseAnswer } from '@/types';
 import inquirer from 'inquirer';
