09afa9797202511fd7ffa7d561d85831b56e84c5
Daniel Barclay <barclaysd@me.com>
2020-12-31 19:24:12 +0000

    Get release information from user and push git tags. Add inquirer and shelljs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { readdirSync, readFileSync } from 'fs';
-import { PackageJson } from '@/types';
+import { PackageJson, ReleaseAnswer } from '@/types';
+import inquirer from 'inquirer';
+import { ScriptService } from '@/services/ScriptService';
 
 const isPackageJsonInCwd = () =>
   readdirSync(process.cwd()).includes('package.json');
+
+const LAST_COMMIT_MESSAGE = `git reflog -1 | sed 's/^.*: //'`;
+
+const blueStdout = (output: string) => `echo -e "\\033[34m${output}\\033[m"`;
 
 const packageJson = (): PackageJson | undefined => {
   try {
@@ -17,10 +23,34 @@ const packageJson = (): PackageJson | undefined => {
   }
 };
 
+const gitTag = ({ message, version }: ReleaseAnswer) => {
+  ScriptService.run(`git tag -a ${version} -m "${message}"`);
+  ScriptService.runSilent('git push --follow-tags');
+  ScriptService.run(blueStdout('Release published!'));
+};
+
 (async () => {
   let version: string | undefined;
   if (isPackageJsonInCwd()) {
     version = packageJson()?.version;
   }
-  console.log(version);
+  const { stdout: commitMessage } = ScriptService.runSilent(
+    LAST_COMMIT_MESSAGE,
+  );
+  const formattedCommitMessage = commitMessage.replace(/(\r\n|\n|\r)/gm, '');
+  const answer = await inquirer.prompt<ReleaseAnswer>([
+    {
+      type: 'input',
+      message: 'Release version',
+      name: 'version',
+      default: () => version,
+    },
+    {
+      type: 'input',
+      message: 'Release message',
+      name: 'message',
+      default: () => formattedCommitMessage,
+    },
+  ]);
+  gitTag(answer);
 })();
